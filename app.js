2be4abe8c57f922d87bfe0474fffe3db67f33ee7
Shravanss1 <shravan12345.sk@gmail.com@.com>
2024-11-21 17:33:11 +0530

    second

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const schoolRoutes = require('./routes/schoolRoutes');
-
+// hello
 const app = express();
 
 app.use(bodyParser.json());
